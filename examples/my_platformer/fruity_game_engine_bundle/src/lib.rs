02309dfc6b1e44116a782ef3d3060bce980aee6b
Léo Le Sommer <llesommer.dev@gmail.com>
2022-12-05 01:55:49 +0100

    temp

--- examples/my_platformer/fruity_game_engine_bundle/src/lib.rs
+++ examples/my_platformer/fruity_game_engine_bundle/src/lib.rs
@@ -1,3 +1,7 @@
 pub use fruity_ecs::*;
 pub use fruity_game_engine::*;
 pub use fruity_hierarchy::*;
+
+pub extern crate fruity_ecs;
+pub extern crate fruity_game_engine;
+pub extern crate fruity_hierarchy;
